990a8ace152d6b88e08d2811467f6114f5466e2c
Zain Ali <recluzedev@gmail.com>
2026-01-06 19:59:41 +0500

    Added the ProductController and Schema of Product inside the create_products_table and make view inside folder of products and inside make UI of products

--- database/migrations/2026_01_05_170845_create_products_table.php
+++ database/migrations/2026_01_05_170845_create_products_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->decimal('Price', 8,2);
         });
     }
 
